45c46e794c74196f8e8486ce353c428656ec7ed4
Camille Pinault <pinaultcamille00@gmail.com>
2025-03-19 13:21:12 +0100

    affichage

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, render_template
+import csv
+from flask import Blueprint, render_template,jsonify
+from app.models import Product
 
 bp = Blueprint('main', __name__)
 
 # Route d'accueil
 @bp.route('/')
 def index():
-    return render_template('index.html', name="Camille&Co")  # Rendre le template index.html
+    products = []
+    with open('app/output/products.csv', newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            product = Product.from_csv_row(row)
+            products.append(product.convert_to_dic())
+    return render_template('index.html', products=products) 
+
+def get_products():
+    products = []
+    with open('app/output/products.csv', newline='', enconding='utf-8') as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        for row in csvreader:
+            product = Product.from_csv_row(row)
+            products.append(product.convert_to_dic())
+    return jsonify(products)
 
 @bp.route('/about')
 def about():
     return render_template('about.html')
+
+@bp.route('/productSheet')
+def productSheet():
+    return render_template('productSheet.html')
 
 if __name__ == '__main__':
     bp.run(debug=True)
