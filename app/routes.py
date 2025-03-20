733744a94f05e4d4e30f29c4d58e818698803639
KeBardoula <kebardoula@gmail.com>
2025-03-20 12:14:51 +0100

    pagination + recherche

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,27 @@
 import csv
 from flask import Blueprint, render_template, request
-from app.models import Product
 
 bp = Blueprint('main', __name__)
 
 # Route d'accueil
 @bp.route('/')
 def index():
-    products = []
-    
-    # Charger les produits des fichiers CSV
-    cave_products = load_products_from_csv('app/output/extime_products_cave.csv')
-    parfum_products = load_products_from_csv('app/output/extime_products_parfums.csv')
-    
-    # Combiner les produits
-    products.extend(cave_products)
-    products.extend(parfum_products)
-    
+    products = load_products_from_csv('output/extime_products.csv')
+
+    # Récupérer le terme de recherche
+    search_query = request.args.get('search', '').strip().lower()  # Récupère le terme de recherche depuis l'URL
+
+    # Filtrer les produits en fonction du terme de recherche
+    if search_query:
+        unique_products = {}
+        for product in products:
+            # Créer une clé unique basée sur le nom et d'autres caractéristiques
+            key = f"{product['name'].lower()}_{product.get('volume', '')}_{product.get('brand', '')}"
+            if search_query in product['name'].lower() and key not in unique_products:
+                unique_products[key] = product  # Ajouter le produit à l'ensemble des produits uniques
+
+        products = list(unique_products.values())  # Convertir l'ensemble en liste
+
     # Pagination
     page = request.args.get('page', 1, type=int)  # Récupère le numéro de page depuis l'URL
     per_page = 30  # Nombre de produits par page
@@ -37,7 +42,7 @@ def index():
     if end_page - start_page < max_displayed_pages - 1:
         start_page = max(1, end_page - (max_displayed_pages - 1))
 
-    return render_template('index.html', products=paginated_products, page=page, total_pages=total_pages, start_page=start_page, end_page=end_page)
+    return render_template('index.html', products=paginated_products, page=page, total_pages=total_pages, start_page=start_page, end_page=end_page, search_query=search_query, total_products=total_products)
 
 def load_products_from_csv(filename):
     """Load products from a given CSV file."""
@@ -46,18 +51,12 @@ def load_products_from_csv(filename):
         with open(filename, newline='', encoding='utf-8') as csvfile:
             csvreader = csv.DictReader(csvfile)
             for row in csvreader:
-                product = Product.from_csv_row(row)
-                products.append(product.convert_to_dic())
+                products.append(row)  # Ajoutez directement la ligne au produit
     except FileNotFoundError:
         print(f"Le fichier {filename} n'a pas été trouvé.")
     except Exception as e:
         print(f"Erreur lors du chargement des produits depuis {filename}: {e}")
     return products
-
-def get_products():
-    products = []
-    # Vous pouvez également adapter cette fonction si nécessaire
-    return jsonify(products)
 
 @bp.route('/about')
 def about():
