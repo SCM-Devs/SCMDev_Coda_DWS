9430693c7377c3b90f11a113201434fe693c9d21
Manille Desseaux <manille024@gmail.com>
2025-03-25 15:24:34 +0100

    dernier détails visuelle

--- app/static/js/alert_modal.js
+++ app/static/js/alert_modal.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const scrapBtn = document.querySelector("#scrap-btn")
+    const overlay = document.querySelector("#overlay")
     const alertModal = document.querySelector("#alert-message")
 
     const yesBtn = document.getElementById("yes-alert-btn")
@@ -7,13 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     scrapBtn.addEventListener('click', () => {
         alertModal.style.display = "flex"
+        overlay.style.display = "flex"
     })
 
     yesBtn.addEventListener('click', () => {
         //Lancer le bot de scrap ici
+        
+        alertModal.style.display = "none"
+        overlay.style.display = "none"
     })
 
     noBtn.addEventListener('click', () => {
         alertModal.style.display = "none"
+        overlay.style.display = "none"
     })
 })
