8ca3c5e747f862b0c07553fa1d46664a4cce3319
Camille <pinaultcamille00@gmail.com>
2025-03-25 10:25:19 +0100

    conflict merge resolved

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,10 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-<<<<<<< HEAD
     # Configuration de l'application (optionnel pour cette démo)
     app.config['SECRET_KEY'] = 'votre_clé_secrète'
     
     # Importer les routes
-=======
-    
->>>>>>> 66dcc0d1797c1c6ceb6a944bfb78b1bdb08a7091
     from . import routes
     app.register_blueprint(routes.bp)
     
